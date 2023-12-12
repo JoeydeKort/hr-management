2a4ecbb52fedb315ac79ad104e2cd94d57bbc602
Joey de Kort <89985454+JoeydeKort@users.noreply.github.com>
2023-12-12 11:12:17 +0100

    [add] repo test

--- src/test/java/be/intecbrussel/hrmanagement/repositories/EmployeeRepositoryTest.java
+++ src/test/java/be/intecbrussel/hrmanagement/repositories/EmployeeRepositoryTest.java
@@ -1,12 +1,15 @@
 package be.intecbrussel.hrmanagement.repositories;
 
 import be.intecbrussel.hrmanagement.models.Employee;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 
 @DataJpaTest
 class EmployeeRepositoryTest {
@@ -35,15 +38,40 @@ class EmployeeRepositoryTest {
 
         // given
         employeeRepository.save(employee);
-
+        System.out.println(employee);
         // when
         Employee savedEmployee = employeeRepository.findByEmail(employee.getEmail()).get();
         savedEmployee.setFirstName("Zoë");
         Employee updatedEmployee = employeeRepository.save(savedEmployee);
-
+        System.out.println(savedEmployee);
         // then
         assertThat(updatedEmployee).isNotNull();
         assertThat(updatedEmployee.getFirstName()).isEqualTo("Zoë");
+    }
+
+    @Test
+    public void givenEmployeEmail_whenFindByEmail_then_ReturnEmployeeObj() {
+
+        // given
+        employeeRepository.save(employee);
+        // when
+        Employee foundEmployee = employeeRepository.findByEmail(employee.getEmail()).get();
+        // then
+        assertThat(foundEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenEmployeeName_whenFindByName_then_ReturnEmployeeObj() {
+
+        // given
+        employeeRepository.save(employee);
+        String firstname = employee.getFirstName();
+        // when
+        Employee foundEmployee = employeeRepository.findByName(firstname);
+        System.out.println(foundEmployee);
+        // then
+        assertThat(foundEmployee).isNotNull();
+
     }
 
 
